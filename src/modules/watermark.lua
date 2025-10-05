ff0b335e56e02b5a2be99e07aae9e655d91a4617
barbarianTopG <134774093+barbarianTopG@users.noreply.github.com>
2025-10-06 10:04:16 +1100

    Update watermark.lua

--- src/modules/watermark.lua
+++ src/modules/watermark.lua
@@ -2,7 +2,7 @@
 local Watermark = {}
 
 function Watermark.process(code)
-    return "--[Obfuscated by Hercules v1.6.2 | hercules-obfuscator.xyz/discord | hercules-obfuscator.xyz/source]\n" .. code
+    return code
 end
 
 return Watermark
